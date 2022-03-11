5de592fdc0c2c5841cc62be42a96eba75a29d2b4
Hoai Phong <19110262@student.hcmute.edu.vn>
2022-03-11 20:39:29 +0700

    Project calculator

---
+++ app/src/main/java/hcmute/edu/vn/calculator_10/MainActivity.java
@@ -0,0 +1,18 @@
+package hcmute.edu.vn.calculator_10;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
